061a79251131725553f7ceda591fc858100b5dfe
Himanshu Saini <himanshu.saini.cse@gmail.com>
2026-09-03 12:21:18 +0530

    03-sept-2026

--- index.js
+++ index.js
@@ -73,11 +73,39 @@ const express = require("express");
 
 const app = express();
 
-const tourRoutes = require("./routes/tourRoutes");
+// const tourRoutes = require("./routes/tourRoutes");
 
 app.use(express.json());
 
-app.use("/api", tourRoutes);
+// app.use("/api", tourRoutes);
+
+// const middleware = (req, res, next) => {
+//   console.log("Middleware executed");
+//   next();
+// }
+
+// const logger = (req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// }
+
+// app.use(logger);
+
+const checkAge = (req,res,next)=>{
+    const age = req.query.age;
+    if(age < 18){
+      return res.status(403).json({message:"you are not allowed to access this resource"});
+
+    }
+    next();
+  
+};
+
+app.use(checkAge);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
